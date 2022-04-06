a78b23839b245039971c17c9f4c5645342f6c1ca
faruk-stgz <74643402+faruk-stgz@users.noreply.github.com>
2022-04-05 23:43:45 -0500

    Update Total Sum.sql

--- Total Sum.sql
+++ Total Sum.sql
@@ -1,14 +1,30 @@
 /*The total number of PCR tests performed as of yesterday in the United States.
   -Author: Faruk Dziho
   -Date: 4/5/2022
-  -Version: 1 
+  -Version: 1.1 
  */
 
 
-DECLARE @asofyesterday as Date = (Select max(Cov.date) from Torch.CovidInfo Cov where Cov.Date < GETDATE()) 
+/*DECLARE @asofyesterday as Date = (Select max(Cov.date) from Torch.CovidInfo Cov where Cov.Date < GETDATE()) 
+@asofyesterday = Getting the most recent date from the table since there might not be any records inserted on the weekend or insert might be lagging 
+this is the wrong approach - some states are few days behind and their sums would be missed looking only at sums at @asofyesterday
 
-/*@asofyesterday = Getting the most recent date from the table since there might not be any records inserted on the weekend or insert might be lagging */
+Instead creating distinct state list with most recent dates and using it to join down in the query
+*/
+
+
+
+; with cte as ( Select distinct state , max(date) as maxdate
+				from Torch.CovidInfo CI
+				where date < getdate()
+				group by state)
+
+
 
 SELECT sum(total_results_reported) as SumOfAllResults
 FROM Torch.CovidInfo CI
-WHERE date = @asofyesterday
+join cte c2 on ci.date = c2.maxdate
+			      and ci.state = c2.state
+
+
+
